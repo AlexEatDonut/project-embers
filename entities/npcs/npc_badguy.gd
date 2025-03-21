fa4f38966b8dca05f479c8396960f14d1d5fccc8
AlexEatDonut <alexeatdonut@gmail.com>
2025-03-21 17:23:22 +0100

    adapted badguy to be bare bones

--- entities/npcs/npc_badguy.gd
+++ entities/npcs/npc_badguy.gd
@@ -2,18 +2,10 @@
 extends CharacterBody3D
 
 @onready var stats = $EnemyStats
-@onready var hitbox = $Hitbox
-
-@onready var head = $NpcHeadLocation
-@onready var headXaxis = $NpcHeadLocation/headX
-@onready var raycast = $NpcHeadLocation/headX/RayCast3D
-@onready var eyes = $Eyes
-
-@onready var muzzle = $NpcHeadLocation/headX/muzzle
 
 @onready var shoottimer = $ShootTimer
 
-@onready var projectile = preload("res://entities/weapons/projectile.tscn")
+#@onready var projectile = preload("res://entities/weapons/projectile.tscn")
 
 var target
 
@@ -27,8 +19,8 @@ enum {
 var state = IDLE
 
 func _ready():
-	head.top_level = true
-	#pass
+
+	pass
 
 func _process(delta):
 
@@ -36,17 +28,18 @@ func _process(delta):
 		IDLE:
 			pass
 		ALERT:
+			print("enemy alerted")
 			#Code : the head follows the player
 			#head.look_at(target.global_transform.origin + Vector3.UP, Vector3.UP)
 			
 			#Code : the eyes follow the player and change the head rotation (preferable i think)
-			eyes.look_at(target.global_transform.origin, Vector3.UP)
-			head.rotate_y(deg_to_rad(eyes.rotation.y * TURN_SPEED))
+			#eyes.look_at(target.global_transform.origin, Vector3.UP)
+			#head.rotate_y(deg_to_rad(eyes.rotation.y * TURN_SPEED))
 			#TODO : This barely works
-			headXaxis.rotation.x = eyes.rotation.x
+			#headXaxis.rotation.x = eyes.rotation.x
 			#headXaxis.rotate_x(deg_to_rad(eyes.rotation.x * TURN_SPEED))
 			#TODO : MAKE THE BODY FOLLOW THE EYES
-			rotate_y(deg_to_rad(eyes.rotation.y * TURN_SPEED))
+			#rotate_y(deg_to_rad(eyes.rotation.y * TURN_SPEED))
 
 func _on_sight_range_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
@@ -57,17 +50,6 @@ func _on_sight_range_body_entered(body: Node3D) -> void:
 func _on_sight_range_body_exited(body: Node3D) -> void:
 	state = IDLE
 	shoottimer.stop()
-
-func _on_shoot_timer_timeout() -> void:
-	if raycast.is_colliding():
-		var hit = raycast.get_collider()
-		if hit.is_in_group("Player"):
-			print("has a fair shot!")
-			var bullet = projectile.instantiate()
-			bullet.player_affiliation = false
-			muzzle.add_child(bullet)
-			
-			
 
 
 #HEALTH RELATED CODE
