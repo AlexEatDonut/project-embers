fa15c9f03080ae80e8884c8e22e94cc664347a65
AlexEatDonut <alexeatdonut@gmail.com>
2025-04-04 16:24:09 +0200

    added critical damage to enemies

--- entities/npcs/npc_badguy.gd
+++ entities/npcs/npc_badguy.gd
@@ -10,6 +10,7 @@ extends CharacterBody3D
 
 @onready var projectile = preload("res://entities/systems/projectile_raycast.tscn")
 @onready var enemy_detector = preload("res://entities/systems/enemy_detector.tscn")
+
 @onready var enemy_stats: Node = $EnemyStats
 
 @onready var character_mesh: MeshInstance3D = $Body/MeshInstance3D
@@ -61,8 +62,10 @@ func _process(delta):
 			pass
 
 func look_at_player(target):
-	body.look_at(target.global_transform.origin, Vector3.UP)
-	#ray_to_player.target_position = Playerinfo.playerLocation
+	var target_position = target.global_transform.origin
+	body.look_at(target_position, Vector3.UP)
+	ray_to_player.look_at(target_position, Vector3.UP)
+	
 
 func looking_around(delta):
 	rotation_degrees += alert_rotation_speed  * delta
@@ -74,14 +77,17 @@ func looking_around(delta):
 
 #region HEALTH RELATED CODE
 func _decrease_health(value):
-	stats.health -= value
-	print("enemy lost health")
-	#the game attempts to start a grace period but fails due to the raycast still being in his hitbox, 
-	#preventing any changes to the monitoring to happen
-		#TODO : Find a way to accomplish this idea
-		#hitbox.start_graceperiod(1)
-		#print("processed starting graceperiod of 1 sec")
-
+	if state != DYING :
+		if enemy_stats.in_critical_hit_range == true :
+			value = value * enemy_stats.crit_factor
+			var sfx_crit_hit = preload("res://sounds/ui/effects/critical_impact.tscn").instantiate()
+			get_tree().current_scene.add_child(sfx_crit_hit)
+		stats.health -= value
+		#the game attempts to start a grace period but fails due to the raycast still being in his hitbox, 
+		#preventing any changes to the monitoring to happen
+			#TODO : Find a way to accomplish this idea
+			#hitbox.start_graceperiod(1)
+			#print("processed starting graceperiod of 1 sec")
 
 func _on_enemy_stats_no_health() -> void:
 	#create_death_effect() 
