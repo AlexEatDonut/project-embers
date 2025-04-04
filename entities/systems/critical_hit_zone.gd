1a53f494c7a88265e40f92de8e42734b28e34da9
AlexEatDonut <alexeatdonut@gmail.com>
2025-04-04 16:24:30 +0200

    added critical hit zone

---
+++ entities/systems/critical_hit_zone.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node3D) -> void:
+	var target = body
+	if target.is_in_group("NPC"):
+		target.enemy_stats.in_critical_hit_range = true
+
+func _on_body_exited(body: Node3D) -> void:
+	var target = body
+	if target.is_in_group("NPC"):
+		target.enemy_stats.in_critical_hit_range = false
