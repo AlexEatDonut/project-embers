33f7d331f47400d0dbd537337487dab0a7836440
AlexEatDonut <alexeatdonut@gmail.com>
2025-08-07 19:08:45 +0200

    edited old item entity to be usable

--- entities/items/pickups/item_medkit_small.gd
+++ entities/items/pickups/item_medkit_small.gd
@@ -1,24 +1,40 @@
 extends Node3D
 
+
+
 @export var healthRatio = 0.25
 
 #@onready var triggerBox = $entityCollisionHull/CollisionShape3D
 @onready var kitSprite = $healthkit_Sprite
 @onready var collisionHull = $entityCollisionHull
+@onready var kit_respawn_timer: Timer = $kit_respawn_timer
+
+@onready var item_respawn_sfx: AudioStreamPlayer = $item_respawn_sfx
+@onready var item_pickup_sfx: AudioStreamPlayer = $item_pickup_sfx
+
+@onready var canInteract = true
 
 func _ready():
 	pass
 
 func _pickup_healthReturn(healthGiven):
-	collisionHull.start_pickupRespawn(8)
 	Playerinfo.increase_health(healthGiven, healthRatio)
 
 func _on_entity_collision_hull_body_entered(body: Node3D) -> void:
 	if Playerinfo.health < Playerinfo.max_health :
 		_pickup_healthReturn(healthRatio)
+		item_pickup_sfx.play()
+		canInteract = false
+		_on_pickup_respawn_start()
 
-func _on_entity_collision_hull_pickup_respawn_start() -> void:
+func _on_pickup_respawn_start() -> void:
 	kitSprite.visible = false
+	kit_respawn_timer.start()
 
-func _on_entity_collision_hull_pickup_respawn_end() -> void:
+func _on_pickup_respawn_end() -> void:
 	kitSprite.visible = true
+	canInteract = true
+	item_respawn_sfx.play()
+
+func _on_kit_respawn_timer_timeout() -> void:
+	_on_pickup_respawn_end()
